25578c4c21ec1652449ad1cffc49c5fe530ecce7
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-10-12 11:26:59 +0300

    added notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { ToastContainer } from 'react-toastify';
 import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/Contacts/ContactList';
 import Container from './App.styled';
 
+
 const App = () => {
   return (
     <Container>
+      <ToastContainer/>
       <h1>Phonebook</h1>
        <ContactForm/>
       <h2>Contacts</h2>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import showWarningNotification from './notification';
+
 /*function for checking the presence of a contact with the specified name*/
 export const availabilityСheck = (contactsArr, name) => {
   let isAvailable = false;
@@ -6,7 +8,7 @@ export const availabilityСheck = (contactsArr, name) => {
   );
 
   if (existingСontact) {
-    alert(`${name} is already in contacts`);
+    showWarningNotification(`${name} is already in contacts`);
     isAvailable = true;
   }
   return isAvailable;
