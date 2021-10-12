e6f2b28b0441296e25a4c373c244a77a9919376a
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-10-12 11:04:12 +0300

    added memoization

--- src/redux/contacts/counter-selectors.js
+++ src/redux/contacts/counter-selectors.js
@@ -1,12 +1,23 @@
+import { createSelector } from 'reselect';
+
 export const getItems = state => state.contacts.items;
 
 export const getFilter = state => state.contacts.filter;
 
-export const getFilteredItems = state => {
-  const items = getItems(state);
-  const filter = getFilter(state);
+export const getFilteredItems = createSelector(
+  [getItems, getFilter],
+  (items, filter) => {
+    return items.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  },
+);
 
-  return items.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase()),
-  );
-};
+// export const getFilteredItems = state => {
+//   const items = getItems(state);
+//   const filter = getFilter(state);
+//   console.log('ререндер');
+//   return items.filter(({ name }) =>
+//     name.toLowerCase().includes(filter.toLowerCase()),
+//   );
+// };
